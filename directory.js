4011c888e5f0282e5e26c2e75fcebb8b5353ef21
GreenCord <steve@nevizen.com>
2019-06-08 21:59:40 -0400

    Initial coding. Needs styles.

---
+++ directory.js
@@ -0,0 +1,114 @@
+(function() {
+  const endpoint =
+    "https://randomuser.me/api/?results=27&nat=US,CA&exc=id,login,registered,dob,gender";
+  const storageKey = "JS_DIRECTORY";
+  // Check local storage for cached data.
+  let directory = JSON.parse(localStorage.getItem(storageKey));
+  if (directory) {
+    console.log("LOADING STORED DIRECTORY...");
+    renderDirectory(directory);
+  } else {
+    console.log("AJAX REQUEST TO GET DATA...");
+    // XMLHttpRequest method.
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+      console.error("httpRequest not initialized.");
+      return false;
+    }
+    httpRequest.onreadystatechange = handleResponse;
+    httpRequest.open("GET", endpoint);
+    httpRequest.send();
+
+    function handleResponse() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          const data = httpRequest.responseText;
+          localStorage.setItem(storageKey, data);
+          renderDirectory(JSON.parse(data));
+        } else {
+          console.error(
+            "httpRequest received with error code: ",
+            httpRequest.status
+          );
+        }
+      }
+    }
+  }
+  function renderDirectory(directory) {
+    console.log("renderDirectory data", directory);
+
+    const ul = document.querySelector("ul.cardlist");
+
+    function generateImage(person, size) {
+      const imgDiv = document.createElement("div");
+      imgDiv.setAttribute("class", "card-image");
+
+      const img = document.createElement("IMG");
+      img.setAttribute("src", person.picture[size]);
+      img.setAttribute("alt", `${person.name.first} ${person.name.last}`);
+
+      imgDiv.appendChild(img);
+      return imgDiv;
+    }
+
+    function generateName(person) {
+      const { title, first, last } = person.name;
+      const div = document.createElement("div");
+      div.setAttribute("class", "card-name");
+      div.textContent = `${first} ${last}`;
+      return div;
+    }
+    function generateElement(elem, className, text) {
+      const element = document.createElement(elem);
+      element.setAttribute("class", className);
+      if (text) element.textContent = text;
+      return element;
+    }
+
+    function generateInfo(person) {
+      const { phone, email } = person,
+        { street, city, state, postcode } = person.location;
+      // Address Element
+      const addr = generateElement("addr", "card-address");
+      // Street Address
+      const streetAddress = generateElement("div", "card-street", street);
+      // City, State, ZIP/Postal Code
+      const csz = generateElement(
+        "div",
+        "card-csz",
+        `${city}, ${state} ${postcode}`
+      );
+      addr.appendChild(streetAddress);
+      addr.appendChild(csz);
+      // Contact Info
+      const infoDiv = generateElement("div", "card-contact");
+      const phoneNum = generateElement("div", "card-phone", `Phone: ${phone}`);
+      const emailAddr = document.createElement(
+        "div",
+        "card-email",
+        `Email: ${email}`
+      );
+      infoDiv.appendChild(phoneNum);
+      infoDiv.appendChild(emailAddr);
+
+      const masterDiv = generateElement("div", "card-info");
+      masterDiv.appendChild(addr);
+      masterDiv.appendChild(infoDiv);
+      return masterDiv;
+    }
+    function renderPerson(person) {
+      const li = document.createElement("li");
+
+      li.appendChild(generateName(person));
+      li.appendChild(generateImage(person, "large"));
+      li.appendChild(generateInfo(person));
+      ul.appendChild(li);
+    }
+
+    const people = directory.results.map(person => person);
+    console.log("people", people);
+    people.forEach(person => {
+      renderPerson(person);
+    });
+  }
+})();

b6afccf34240fdf72559c12e3866c3f5d5c8100a
GreenCord <steve@nevizen.com>
2019-06-10 08:50:21 -0400

    Refactored functions to create cards into separate module. Version functional

--- js/directory.js
+++ js/directory.js
@@ -12,7 +12,6 @@
     function constructUrl(endpoint) {
       let stub = [];
       let params = [];
-      let url;
       for (const key in endpoint) {
         let val = endpoint[key];
         if (endpoint.hasOwnProperty(key)) {
@@ -21,8 +20,9 @@
             : params.push(`${key}=${val.join(",")}`);
         }
       }
-      return (url = `${stub.join("")}?${params.join("&")}`);
+      return `${stub.join("")}?${params.join("&")}`;
     }
+
     // Fetches Data from URL
     function fetchData(endpoint) {
       const url = constructUrl(endpoint);
@@ -39,7 +39,90 @@
       fetchData: fetchData
     };
   })();
+  // ----------------------------------------------------------------------------
+  // Method: renderDirectory | Returns:
+  // ----------------------------------------------------------------------------
+  // Info:
+  // ----------------------------------------------------------------------------
+  const handleData = (function renderDirectory(directory) {
+    // Function taking the person phone data and normalizing the format.
+    // Returns empty string or string formatted to: (###)###-####.
+    function normalizePhone() {
+      let phone = this.replace(/\D/g, "");
+      phone = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
+      if (phone) {
+        return `(${phone[1]})${phone[2]}-${phone[3]}`;
+      }
+      return "";
+    }
+    // Function call with person to generate a persons image.
+    // Size can be 'large', 'medium', or 'thumbnail'.
+    function generatePersonImage(size) {
+      const imgDiv = document.createElement("div");
+      imgDiv.setAttribute("class", "card-image");
 
+      const img = document.createElement("IMG");
+      img.setAttribute("src", this.picture[size]);
+      img.setAttribute("alt", `${this.name.first} ${this.name.last}`);
+
+      imgDiv.appendChild(img);
+      return imgDiv;
+    }
+    // Function to generate a simple element with a class. Text is optional.
+    function generateElement(elem, className, text) {
+      const element = document.createElement(elem);
+      element.setAttribute("class", className);
+      if (text) element.textContent = text;
+      return element;
+    }
+    // Function to generate contact info from a person.
+    function generateInfo() {
+      const { phone, email } = this;
+      const formattedPhone = normalizePhone.call(phone);
+      // Contact Info
+      const infoDiv = generateElement("div", "card-contact");
+      const phoneNum = generateElement("div", "card-phone", formattedPhone);
+      const emailAddr = generateElement("div", "card-email", email);
+      infoDiv.appendChild(phoneNum);
+      infoDiv.appendChild(emailAddr);
+
+      const masterDiv = generateElement("div", "card-info");
+      masterDiv.appendChild(infoDiv);
+      return masterDiv;
+    }
+    // Function to render the full person card.
+    function renderPersonCard() {
+      const ul = document.querySelector("ul.card-list");
+      const li = document.createElement("li");
+      li.setAttribute("class", "card-item");
+      const name = generateElement(
+        "div",
+        "card-name",
+        `${this.name.first} ${this.name.last}`
+      );
+      name.appendChild(generatePersonImage.call(this, "large"));
+      li.appendChild(name);
+
+      // li.appendChild(generatePersonImage.call(this, "large"));
+      li.appendChild(generateInfo.call(this));
+      ul.appendChild(li);
+    }
+    // Extract people from dataset and render their cards.
+    function createDirectory(directory) {
+      return directory.map(person => {
+        renderPersonCard.call(person);
+        return person;
+      });
+    }
+
+    return {
+      createDirectory: createDirectory
+    };
+  })();
+
+  // ----------------------------------------------------------------------------
+  // Page Logic
+  // ----------------------------------------------------------------------------
   const config = {
     url: "https://randomuser.me/api/",
     results: [27],
@@ -48,7 +131,8 @@
   };
 
   const data = getData.fetchData(config);
-  data.then(response => console.log(response));
+  const directory = handleData;
+  data.then(response => directory.createDirectory(response.results));
 })();
 
 // IIFE to ensure contained variables and function calls.
@@ -86,74 +170,74 @@
 //       }
 //     }
 //   }
-//   function renderDirectory(directory) {
-//     // Function taking the person phone data and normalizing the format.
-//     // Returns empty string or string formatted to: (###)###-####.
-//     function normalizePhone() {
-//       let phone = this.replace(/\D/g, "");
-//       phone = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
-//       if (phone) {
-//         return `(${phone[1]})${phone[2]}-${phone[3]}`;
-//       }
-//       return "";
+// function renderDirectory(directory) {
+//   // Function taking the person phone data and normalizing the format.
+//   // Returns empty string or string formatted to: (###)###-####.
+//   function normalizePhone() {
+//     let phone = this.replace(/\D/g, "");
+//     phone = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
+//     if (phone) {
+//       return `(${phone[1]})${phone[2]}-${phone[3]}`;
 //     }
-//     // Function call with person to generate a persons image.
-//     // Size can be 'large', 'medium', or 'thumbnail'.
-//     function generatePersonImage(size) {
-//       const imgDiv = document.createElement("div");
-//       imgDiv.setAttribute("class", "card-image");
-
-//       const img = document.createElement("IMG");
-//       img.setAttribute("src", this.picture[size]);
-//       img.setAttribute("alt", `${this.name.first} ${this.name.last}`);
-
-//       imgDiv.appendChild(img);
-//       return imgDiv;
-//     }
-//     // Function to generate a simple element with a class. Text is optional.
-//     function generateElement(elem, className, text) {
-//       const element = document.createElement(elem);
-//       element.setAttribute("class", className);
-//       if (text) element.textContent = text;
-//       return element;
-//     }
-//     // Function to generate contact info from a person.
-//     function generateInfo() {
-//       const { phone, email } = this;
-//       const formattedPhone = normalizePhone.call(phone);
-//       // Contact Info
-//       const infoDiv = generateElement("div", "card-contact");
-//       const phoneNum = generateElement("div", "card-phone", formattedPhone);
-//       const emailAddr = generateElement("div", "card-email", email);
-//       infoDiv.appendChild(phoneNum);
-//       infoDiv.appendChild(emailAddr);
-
-//       const masterDiv = generateElement("div", "card-info");
-//       masterDiv.appendChild(infoDiv);
-//       return masterDiv;
-//     }
-//     // Function to render the full person card.
-//     function renderPersonCard() {
-//       const ul = document.querySelector("ul.card-list");
-//       const li = document.createElement("li");
-//       li.setAttribute("class", "card-item");
-//       const name = generateElement(
-//         "div",
-//         "card-name",
-//         `${this.name.first} ${this.name.last}`
-//       );
-//       name.appendChild(generatePersonImage.call(this, "large"));
-//       li.appendChild(name);
-
-//       // li.appendChild(generatePersonImage.call(this, "large"));
-//       li.appendChild(generateInfo.call(this));
-//       ul.appendChild(li);
-//     }
-
-//     // Extract people from dataset and render their cards.
-//     const people = directory.results.map(person => {
-//       renderPersonCard.call(person);
-//       return person;
-//     });
+//     return "";
 //   }
+//   // Function call with person to generate a persons image.
+//   // Size can be 'large', 'medium', or 'thumbnail'.
+//   function generatePersonImage(size) {
+//     const imgDiv = document.createElement("div");
+//     imgDiv.setAttribute("class", "card-image");
+
+//     const img = document.createElement("IMG");
+//     img.setAttribute("src", this.picture[size]);
+//     img.setAttribute("alt", `${this.name.first} ${this.name.last}`);
+
+//     imgDiv.appendChild(img);
+//     return imgDiv;
+//   }
+//   // Function to generate a simple element with a class. Text is optional.
+//   function generateElement(elem, className, text) {
+//     const element = document.createElement(elem);
+//     element.setAttribute("class", className);
+//     if (text) element.textContent = text;
+//     return element;
+//   }
+//   // Function to generate contact info from a person.
+//   function generateInfo() {
+//     const { phone, email } = this;
+//     const formattedPhone = normalizePhone.call(phone);
+//     // Contact Info
+//     const infoDiv = generateElement("div", "card-contact");
+//     const phoneNum = generateElement("div", "card-phone", formattedPhone);
+//     const emailAddr = generateElement("div", "card-email", email);
+//     infoDiv.appendChild(phoneNum);
+//     infoDiv.appendChild(emailAddr);
+
+//     const masterDiv = generateElement("div", "card-info");
+//     masterDiv.appendChild(infoDiv);
+//     return masterDiv;
+//   }
+//   // Function to render the full person card.
+//   function renderPersonCard() {
+//     const ul = document.querySelector("ul.card-list");
+//     const li = document.createElement("li");
+//     li.setAttribute("class", "card-item");
+//     const name = generateElement(
+//       "div",
+//       "card-name",
+//       `${this.name.first} ${this.name.last}`
+//     );
+//     name.appendChild(generatePersonImage.call(this, "large"));
+//     li.appendChild(name);
+
+//     // li.appendChild(generatePersonImage.call(this, "large"));
+//     li.appendChild(generateInfo.call(this));
+//     ul.appendChild(li);
+//   }
+
+//   // Extract people from dataset and render their cards.
+//   const people = directory.results.map(person => {
+//     renderPersonCard.call(person);
+//     return person;
+//   });
+// }
 // })();
